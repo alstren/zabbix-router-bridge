191dfe43d9f6fd24b1ab3ad3ebbc3c885293de8b
Виталий Андреев <vitalii.andreev@redlab.dev>
2024-02-05 23:18:46 +0500

    Start project

---
+++ src/main/java/ru/krsmon/zabbixrouterbridge/ZabbixRouterBridgeApplication.java
@@ -0,0 +1,13 @@
+package ru.krsmon.zabbixrouterbridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZabbixRouterBridgeApplication {
+
+	public static void main(String... args) {
+		SpringApplication.run(ZabbixRouterBridgeApplication.class, args);
+	}
+
+}
